67c0951a8d63be5d80c7745d2647e01a655ef57e
aamankumaar <amanroy7481@gmail.com>
2025-02-14 08:46:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menu_btn");
+const navMenu = document.querySelector(".nav_links");
+
+menuBtn.addEventListener('click',()=>
+{
+    navMenu.classList.toggle("mobile_menu")
+})
